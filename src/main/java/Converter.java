61ab6e17bf8dfd2bf2368fde91e98ce5ab9b0d57
kochuv <veronika.kochugova@gmail.ru>
2018-06-13 19:39:44 +0300

    tabs refactoring

--- src/main/java/Converter.java
+++ src/main/java/Converter.java
@@ -18,11 +18,15 @@ class Converter {
     private final String STEP_TYPE_SEQ = "sequential";
     private final String STEP_TYPE_LOAD = "load";
     private final String STEP_TYPE_PARALLEL = "parallel";
+    private final String STEP_TYPE_PRECONDITION = "precondition";
+
     private final String KEY_STEPS = "steps";
     private final String KEY_TYPE = "type";
     private final String KEY_CONFIG = "config";
     private final String KEY_VALUE = "value";
     private final String KEY_IN = "in";
+
+    private final String TAB = "    ";
 
     public Converter(Path oldScenarioPath) throws IOException {
         jsonScenario = new JSONScenario(oldScenarioPath.toFile());
@@ -42,13 +46,19 @@ class Converter {
                 }
                 break;
                 case STEP_TYPE_SEQ: {
-                    print(tab + " ", (ArrayList<Object>) tree.get(KEY_STEPS));
+                    print(tab + TAB, (ArrayList<Object>) tree.get(KEY_STEPS));
                 }
                 break;
                 case STEP_TYPE_PARALLEL: {
-                    print(tab + " ", (ArrayList<Object>) tree.get(KEY_STEPS));
+                    System.out.println("\n<PARALLEL> : {");
+                    print(tab + TAB, (ArrayList<Object>) tree.get(KEY_STEPS));
+                    System.out.println(tab + "}");
                 }
                 break;
+                case STEP_TYPE_PRECONDITION: {
+                    String str = createPrecondStep(tab, (Map<String, Object>) tree.get(KEY_CONFIG));
+                    System.out.print("\n" + str + "\n");
+                } break;
                 case STEP_TYPE_FOR: {
 
                     final List inValue = (List) tree.get(KEY_IN);
@@ -58,7 +68,8 @@ class Converter {
                         System.out.print("\n" + str + "\n");
                     }
 
-                    print(tab + " ", (ArrayList<Object>) tree.get(KEY_STEPS));
+                    print(tab + TAB, (ArrayList<Object>) tree.get(KEY_STEPS));
+                    System.out.println(tab + "}");
                 }
                 break;
                 case STEP_TYPE_LOAD: {
@@ -70,6 +81,13 @@ class Converter {
                     System.out.println(tab + "<" + tree.get(key) + ">");
             }
         }
+    }
+
+    private String createPrecondStep(String tab, Map<String,Object> config) {
+        String str = tab + "var step_" + (++stepCounter) + " = PreconditionLoad.config( { "
+                + config + "});\n";
+        str += tab + "step_" + (stepCounter) + ".start();";
+        return str;
     }
 
     private void print(final String tab, final ArrayList<Object> steps) {
@@ -99,8 +117,8 @@ class Converter {
     }
 
     public String createStepLoad(String tab, Map<String, Object> config) {
-        String str = tab + "var step_" + (++stepCounter) + " = Load.config( { type: \""
-                + config + "\"});\n";
+        String str = tab + "var step_" + (++stepCounter) + " = Load.config( { "
+                + config + "});\n";
         str += tab + "step_" + (stepCounter) + ".start();";
         return str;
     }
