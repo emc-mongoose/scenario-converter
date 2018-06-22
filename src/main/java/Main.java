b1e4ada1f7d8e844cf9c9f83245628e3653c83d8
kochuv <veronika.kochugova@gmail.ru>
2018-06-22 14:05:01 +0300

    fixed #15

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,8 +5,6 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         for (String arg : args) {
-            System.out.println("\n\n---------------------------------------------------------------------------------");
-            System.out.println(arg + "\n");
             ScenarioConverter.print(new Scenario(Scenario.parseJson(Paths.get(arg).toFile())));
         }
         ScenarioConverter.print(new Scenario(Scenario.parseJson(Paths.get("example", "scenario", "json", "misc", "rampup.json").toFile())));
